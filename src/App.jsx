dfbaea1218cb5160c9da3c5c45daa086fa99dd46
jesus palomino <davjes1003@gmail.com>
2022-07-27 09:05:55 -0500

    cards publications advance

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import UserCard from "./Components/UserCard";
 import MyTeam from "./Components/MyTeam";
 import CardsPublications from "./Components/CardsPublications";
+import React, { useEffect } from "react";
+
 function App() {
-  let urlList = fetch()
+  const [posts, setPosts] = React.useState([]);
+  useEffect(() => {
+    obtenerDatos();
+  }, []);
+  const obtenerDatos = async () => {
+    const post = await fetch("https://my-daily-bootcamp.herokuapp.com/posts.json");
+    const infoPost = await post.json();
+    console.log(infoPost);
+    setPosts([...infoPost]);
+  }
   return (
     <div className="bg-gray-400">
       <Navbar />
       <UserCard />
       <MyTeam/>
-      <CardsPublications urlList = {["https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg", "https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg"]}/>
-      <CardsPublications urlList = {["https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg"]}/>
-      <CardsPublications urlList = {["https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg", "https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg", "https://my-daily-bootcamp.netlify.app/assets/feed/cups-1.jpg"]}/>
+      {posts.map ((post)=>{
+        return <CardsPublications post={post}>
+        </CardsPublications>
+      })}
     </div>
   );
 }
